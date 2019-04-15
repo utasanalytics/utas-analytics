19a45e7f99c8ec2cca3ca476792ad297ea9545f9
Zuejinn <32529254+Zuejinn@users.noreply.github.com>
2019-04-15 15:03:37 +1000

    Start

--- faq.php
+++ faq.php
@@ -1,3 +1,12 @@
+<!--Contains basic starting html code-->
 <?php include("Headers/header.php");?>
-    
+<!--Insert Your CSS include statement here-->
+
+<!--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^-->
+
+
+
+
+
+<!--Contains closing tags for body and html-->
 <?php include("Footers/footer.php");?>
