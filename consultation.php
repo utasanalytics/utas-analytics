fe068c2e389621cab1b8082cafa8f36054e68fc5
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2019-04-23 15:05:15 +1000

    Update consultation.php
    Done consultation. Not too sure what to do for admn page, so addedsome buttons atm.

--- consultation.php
+++ consultation.php
@@ -4,6 +4,60 @@
 
 <!--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^-->
 
+<div class="container">
+		<!-- The sidebar -->
+		<div class="sidebar">
+			<h3 class="title">ANALYTICS BICT & MITS </h3>
+			<ul>
+				<li><a href="">Unit Explorer</a></li>
+				<li><a href="faq.html">FAQ</a></li>
+				<li><a href="">Consultation</a></li>
+				<li><a href="">Profile</a></li>
+			</ul>
+		</div>
+
+		<!-- The main section for FAQ-->
+		<div class="main">
+
+			<!-- The Login for the FAQ section  -->
+			<div class="login"><button type="submit">Login/Logout</button></div>
+
+
+            <!-- The content for the FAQ section  -->
+			<div class="content">
+				
+				<h2>Book a Consultation:</h2>
+				
+				<form>
+					<b>Staff Name:</b>
+					<select name="staff">
+						<option>Soonja</option>
+						<option>Erin</option>
+						<option>Matthew</option>
+					</select><br><br>
+
+					<b>Time:</b>
+					<input type="Time" name="time"><br><br>
+
+					<b>Location: </b>
+					<select name="location">
+						<option>Hobart</option>
+						<option>Launceston</option>
+					</select><br><br>
+
+					<b>Utas ID: </b>
+					<input type="text" name="id"><br><br>
+
+					<b>Email: </b>
+					<input type="Email" name="id"><br><br>
+
+					<input type="submit" value="submit">
+
+				</form>
+
+			</div>
+			
+		</div>
 
 
 
