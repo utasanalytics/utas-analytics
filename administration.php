ae6c7adbfc510849f3ea96458b2cda263892484a
IronHarambe <49575350+IronHarambe@users.noreply.github.com>
2019-04-23 15:05:21 +1000

    Update administration.php

--- administration.php
+++ administration.php
@@ -3,7 +3,26 @@
 <!--Insert Your CSS include statement here-->
 
 <!--^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^-->
+<!-- The main section for FAQ-->
+		<div class="main">
 
+			<!-- The Login for the FAQ section  -->
+			<div class="login"><button type="submit">Login/Logout</button></div>
+
+
+            <!-- The content for the FAQ section  -->
+			<div class="content">
+				
+				<h2>The Adminstrator Page:</h2>
+				
+				<button>Add Unit</button>
+				<button>Modify Unit</button>
+				<button>Delete Unit</button><br><br>
+				
+
+			</div>
+			
+		</div>
 
 
 
